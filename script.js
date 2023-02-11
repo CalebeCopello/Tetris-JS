c5b935973c452adf15a1401dd0438396e6a52b7c
CalebeCopello <114925816+CalebeCopello@users.noreply.github.com>
2023-02-10 23:43:53 -0300

    Creation of Tetris-JS repository
    Creation of files and base to tetris game

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const GRID = document.querySelector('grid');
+    let squares = Array.from(document.querySelector('.grid div'));
+    const SCOREDISPLAY = document.querySelector('score');
+    const STARTBTN = document.querySelector('start');
+    const WIDTH = 10;
+});
